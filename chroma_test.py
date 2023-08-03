607c882bb6eb1c777bf5e66810d2c0c888a60a94
shamspias <shamsuddinpias0@gmail.com>
2023-08-03 17:37:14 +0600

    fix: query

--- chroma_test.py
+++ chroma_test.py
@@ -13,6 +13,7 @@ OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
 
+
 csv_loader = CSVLoader(file_path='data/property.csv')
 
 index_creator = VectorstoreIndexCreator()
@@ -20,7 +21,7 @@ document_search = index_creator.from_loaders([csv_loader])
 
 chat_chain = RetrievalQA.from_chain_type(llm=ChatOpenAI(), chain_type="stuff",
                                          retriever=document_search.vectorstore.as_retriever(), input_key="question")
-query_text = "Publication date for this KRN6/2/2/E/30/0/0/7 clime reference id"
+query_text = "Claim Reference ID is KRN6/2/2/E/30/0/0/7 what is Gazette Published Date for it"
 response = chat_chain({"question": query_text})
 
 print(response)
