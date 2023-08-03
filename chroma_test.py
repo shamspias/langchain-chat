1fe1d50536f3ae6dcebf4f8e04adef4cb9261d71
shamspias <shamsuddinpias0@gmail.com>
2023-08-03 16:16:29 +0600

    fix: chroma test value

---
+++ chroma_test.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+
+from langchain.document_loaders import CSVLoader
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv('OPEN_AI_KEY')
+
+csv_loader = CSVLoader(file_path='data/property.csv')
+
+index_creator = VectorstoreIndexCreator(openai_api_key=OPENAI_API_KEY)
+document_search = index_creator.from_loaders([csv_loader])
+
+chat_chain = RetrievalQA.from_chain_type(llm=ChatOpenAI(), chain_type="stuff",
+                                         retriever=document_search.vectorstore.as_retriever(), input_key="question")
+query_text = "Publication date for this KRN6/2/2/E/30/0/0/7 clime reference id"
+response = chat_chain({"question": query_text})
+
+print(response)
+print(response['result'])
